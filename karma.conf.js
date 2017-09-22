22b3d6c8970c294f95042ba172911c197b34b254
Alexander Pope <alex@pope-industries.com>
2017-09-22 10:48:23 +0200

    upgrade test setup

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@ module.exports = function(config) {
   config.set({
     frameworks: ['mocha', 'chai'],
     files: ['test/test-browser.js'],
-    reporters: ['dots'],
+    reporters: ['mocha'],
     port: 9876, // karma web server port
     colors: true,
     logLevel: config.LOG_INFO,
